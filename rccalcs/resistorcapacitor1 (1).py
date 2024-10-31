3cdae426682e91da77c88cf702b26401d8dff29b
DylanBDC <dylan.brett@dcmail.ca>
2024-10-31 18:15:49 -0400

    Update resistorcapacitor1 (1).py
    I have added a header to the file with my name and date

--- rccalcs/resistorcapacitor1 (1).py
+++ rccalcs/resistorcapacitor1 (1).py
@@ -1,3 +1,12 @@
+# Dylan Brett (100933134)
+# TPRG-2131-02
+# Oct 31, 2024
+# This program is strictly my own work. Any material
+# beyond course learning materials that is taken from
+# the Web or other sources is properly cited, giving
+# credit to the original author(s). I havent used any
+# code from other sources other than referncing the course material
+
 """TPRG2131 Winter 202x RC class starter with simplistic test code."""
 
 class ResistorCapacitor (object):
